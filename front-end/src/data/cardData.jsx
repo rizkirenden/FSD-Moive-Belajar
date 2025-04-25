f3f4fd5bc41a028913906cd8250c671d4f201a4b
rizkirenden <rizkirenden@gmail.com>
2025-04-25 23:43:17 +0800

    Update Gambar

---
+++ front-end/src/data/cardData.jsx
@@ -0,0 +1,35 @@
+const cardData = [
+  {
+    id: 1,
+    imageSrc: '/assets/Number=1.png',
+  },
+  {
+    id: 2,
+    imageSrc: '/assets/Number=2.png',
+  },
+  {
+    id: 3,
+    imageSrc: '/assets/Number=3.png',
+  },
+  {
+    id: 4,
+    imageSrc: '/assets/Number=4.png',
+  },
+  {
+    id: 5,
+    imageSrc: '/assets/Number=5.png',
+  },
+  {
+    id: 6,
+    imageSrc: '/assets/Number=6.png',
+  },
+  {
+    id: 7,
+    imageSrc: '/assets/Number=7.png',
+  },
+  {
+    id: 8,
+    imageSrc: '/assets/Number=8.png',
+  },
+];
+export default cardData;
