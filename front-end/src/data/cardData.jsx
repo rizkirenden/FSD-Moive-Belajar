5bd4c989171801856f4fafbffe780496f0280507
rizkirenden <rizkirenden@gmail.com>
2025-04-26 00:09:02 +0800

    update Data Film

--- front-end/src/data/cardData.jsx
+++ front-end/src/data/cardData.jsx
@@ -31,5 +31,77 @@ const cardData = [
     id: 8,
     imageSrc: '/assets/Number=8.png',
   },
+  {
+    id: 9,
+    imageSrc: '/assets/Number=9.png',
+  },
+  {
+    id: 10,
+    imageSrc: '/assets/Number=10.png',
+  },
+  {
+    id: 11,
+    imageSrc: '/assets/Number=11.png',
+  },
+  {
+    id: 12,
+    imageSrc: '/assets/Number=12.png',
+  },
+  {
+    id: 13,
+    imageSrc: '/assets/Number=13.png',
+  },
+  {
+    id: 14,
+    imageSrc: '/assets/Number=14.png',
+  },
+  {
+    id: 15,
+    imageSrc: '/assets/Number=15.png',
+  },
+  {
+    id: 16,
+    imageSrc: '/assets/Number=16.png',
+  },
+  {
+    id: 17,
+    imageSrc: '/assets/Number=17.png',
+  },
+  {
+    id: 18,
+    imageSrc: '/assets/Number=18.png',
+  },
+  {
+    id: 19,
+    imageSrc: '/assets/Number=19.png',
+  },
+  {
+    id: 20,
+    imageSrc: '/assets/Number=20.png',
+  },
+  {
+    id: 21,
+    imageSrc: '/assets/Number=21.png',
+  },
+  {
+    id: 22,
+    imageSrc: '/assets/Number=22.png',
+  },
+  {
+    id: 23,
+    imageSrc: '/assets/Number=23.png',
+  },
+  {
+    id: 24,
+    imageSrc: '/assets/Number=24.png',
+  },
+  {
+    id: 25,
+    imageSrc: '/assets/Number=25.png',
+  },
+  {
+    id: 26,
+    imageSrc: '/assets/Number=26.png',
+  },
 ];
 export default cardData;
