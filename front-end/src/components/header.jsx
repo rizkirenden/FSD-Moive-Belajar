ef54092b6c0d7f138cdef0b3105afca6880b863c
rizkirenden <rizkirenden@gmail.com>
2025-04-26 00:08:30 +0800

    Update Responsive

--- front-end/src/components/header.jsx
+++ front-end/src/components/header.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 function Header({ title }) {
   return (
     <header>
-      <nav className="bg-white p-2 h-12 flex justify-between items-center ml-4 mr-4 mt-4 rounded-full">
-        <div className="flex items-center space-x-4">
+      <nav className="bg-white p-2 h-12 flex justify-between items-center mx-4 mt-4 rounded-full">
+        {/* Logo and Title */}
+        <div className="flex items-center space-x-4 flex-wrap sm:flex-nowrap">
           <img
             src="/assets/baverse.png"
             alt="baverse"
             className="w-16 h-16 object-contain"
           />
-          <h1 className="text-[#4a70db] text-sm">{title}</h1>
+          <h1 className="text-[#4a70db] text-sm sm:text-lg">{title}</h1>
         </div>
-        <ul className="flex space-x-4 mr-4">
+
+        {/* Navigation Links */}
+        <ul className="flex space-x-4 mr-4 text-sm flex-wrap sm:flex-nowrap">
           <li>
-            <Link to="/" className="text-black text-sm">
+            <Link to="/" className="text-black">
               Home
             </Link>
           </li>
           <li>
-            <Link to="/about" className="text-black text-sm">
+            <Link to="/about" className="text-black">
               About
             </Link>
           </li>
@@ -28,4 +32,5 @@ function Header({ title }) {
     </header>
   );
 }
+
 export default Header;
